50dc9ad103d563bbad91123887327779cae00154
Sam Law <samuel.law@system1group.com>
2023-02-03 22:54:49 +0000

    Updated movement to use InputSystem and added dash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 using UnityEngine.InputSystem;
-#endif
+using Assets.Scripts;
 
 namespace Character
 {
     [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
-        public float _speed = 5f;
+        public float MoveSpeed = 5f;
+        public float DashSpeed = 15f;
+        public float SpeedChangeRate = 12.5f;
+
         private CharacterController _controller;
+        private InputControlsInputs _input;
+        private float _speed;
 
 
         // Start is called before the first frame update
         void Start()
         {
             _controller = GetComponent<CharacterController>();
+            _input = GetComponent<InputControlsInputs>();
+            _controller.detectCollisions = true;
         }
 
         // Update is called once per frame
         void Update()
         {
-            float vTranslate = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
-            float hTranslate = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+            Move();
+        }
 
-            _controller.Move(new Vector3(hTranslate, vTranslate, 0));
+        private void Move()
+        {
+            // set target speed based on move speed, sprint speed and if sprint is pressed
+            float targetSpeed = _input.dash ? DashSpeed : MoveSpeed;
+
+            // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+            // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+            // if there is no input, set the target speed to 0
+            if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+
+            // a reference to the players current horizontal velocity
+            float currentHorizontalSpeed = new Vector2(_controller.velocity.x, _controller.velocity.y).magnitude;
+
+            float speedOffset = 0.1f;
+            float inputMagnitude = _input.dash ? 1 : _input.move.magnitude;
+
+            // accelerate or decelerate to target speed
+            if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+            {
+                // creates curved result rather than a linear one giving a more organic speed change
+                // note T in Lerp is clamped, so we don't need to clamp our speed
+                _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
+
+                // round speed to 3 decimal places
+                _speed = Mathf.Round(_speed * 1000f) / 1000f;
+            }
+            else
+            {
+                _speed = targetSpeed;
+            }
+
+            _controller.Move(_input.move * _speed * Time.deltaTime);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("Collision!");
         }
     }
 }
