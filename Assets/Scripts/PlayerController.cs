be70777772fde5af324e63410d8474b1838c6ca2
Sam Law <samuel.law@system1group.com>
2023-02-04 00:17:26 +0000

    Use Physics2d instead of CharacterController & added collision detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,13 @@ using Assets.Scripts;
 
 namespace Character
 {
-    [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
         public float MoveSpeed = 5f;
         public float DashSpeed = 15f;
         public float SpeedChangeRate = 12.5f;
 
-        private CharacterController _controller;
+        private Rigidbody2D _rigidBody;
         private InputControlsInputs _input;
         private float _speed;
 
@@ -19,9 +18,8 @@ namespace Character
         // Start is called before the first frame update
         void Start()
         {
-            _controller = GetComponent<CharacterController>();
+            _rigidBody = GetComponent<Rigidbody2D>();
             _input = GetComponent<InputControlsInputs>();
-            _controller.detectCollisions = true;
         }
 
         // Update is called once per frame
@@ -35,14 +33,11 @@ namespace Character
             // set target speed based on move speed, sprint speed and if sprint is pressed
             float targetSpeed = _input.dash ? DashSpeed : MoveSpeed;
 
-            // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
-
-            // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
             // if there is no input, set the target speed to 0
             if (_input.move == Vector2.zero) targetSpeed = 0.0f;
 
             // a reference to the players current horizontal velocity
-            float currentHorizontalSpeed = new Vector2(_controller.velocity.x, _controller.velocity.y).magnitude;
+            float currentHorizontalSpeed = new Vector2(_rigidBody.velocity.x, _rigidBody.velocity.y).magnitude;
 
             float speedOffset = 0.1f;
             float inputMagnitude = _input.dash ? 1 : _input.move.magnitude;
@@ -51,7 +46,6 @@ namespace Character
             if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
             {
                 // creates curved result rather than a linear one giving a more organic speed change
-                // note T in Lerp is clamped, so we don't need to clamp our speed
                 _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
 
                 // round speed to 3 decimal places
@@ -62,12 +56,18 @@ namespace Character
                 _speed = targetSpeed;
             }
 
-            _controller.Move(_input.move * _speed * Time.deltaTime);
+            // set player's velocity
+            _rigidBody.velocity = _input.move * _speed;
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        void OnTriggerEnter2D(Collider2D collision)
         {
-            Debug.Log("Collision!");
+            Debug.Log("Trigger!");
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            //ToDo: Deal Damage
         }
     }
 }
